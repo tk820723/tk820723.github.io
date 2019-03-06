3b54c874a6c757cac87edf497e45395430a4859e
Weibai Lu <weibai_lu@126.com>
2019-03-06 11:38:54 +0800

    Publish

---
+++ pack.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# 上面中的 #! 是一种约定标记, 它可以告诉系统这个脚本需要什么样的解释器来执行;
+
+echo "cd control dir"
+cd ./Personal_Website_Control
+
+echo
+ls
+
---
+++ readme.md
@@ -0,0 +1,2 @@
+这个website目录结构：
+1. public文件夹是网站的coding 由hexo生成
